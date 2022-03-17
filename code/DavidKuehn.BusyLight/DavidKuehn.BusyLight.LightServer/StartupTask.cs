5fcd91815518a72c566fc2aa6c6262aea12e464d
David Kuehn <kuehnd96@gmail.com>
2022-03-16 21:06:05 -0500

    Create solution for light control. 🌞

---
+++ code/DavidKuehn.BusyLight/DavidKuehn.BusyLight.LightServer/StartupTask.cs
@@ -0,0 +1,18 @@
+﻿using Windows.ApplicationModel.Background;
+
+namespace DavidKuehn.BusyLight.LightServer
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        private BackgroundTaskDeferral deferral;
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            deferral = taskInstance.GetDeferral();
+
+            //
+            // TODO: Insert code to start one or more asynchronous methods; Call deferral.Complete to stop
+            //
+        }
+    }
+}
