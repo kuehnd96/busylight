0ab4af0e59f1ac595743250e84077fefcf630a2c
David Kuehn <kuehnd96@gmail.com>
2022-03-16 21:34:24 -0500

    Try to interact with blinkt light. 🚥

--- code/DavidKuehn.BusyLight/DavidKuehn.BusyLight.LightServer/StartupTask.cs
+++ code/DavidKuehn.BusyLight/DavidKuehn.BusyLight.LightServer/StartupTask.cs
@@ -1,18 +1,49 @@
-﻿using Windows.ApplicationModel.Background;
+﻿using Blinkt;
+using System;
+using Windows.ApplicationModel.Background;
+using Windows.System.Threading;
 
 namespace DavidKuehn.BusyLight.LightServer
 {
     public sealed class StartupTask : IBackgroundTask
     {
-        private BackgroundTaskDeferral deferral;
+        private BackgroundTaskDeferral _deferral;
+        private ThreadPoolTimer _timer;
+        private IBlinkt _blinkt;
+        private bool _isOn;
+        private const int Times = 10;
+        private int ticks = 0;
 
         public void Run(IBackgroundTaskInstance taskInstance)
         {
-            deferral = taskInstance.GetDeferral();
+            _deferral = taskInstance.GetDeferral();
 
-            //
-            // TODO: Insert code to start one or more asynchronous methods; Call deferral.Complete to stop
-            //
+            _blinkt = new Blinkt.Blinkt();
+            _blinkt.SetBrightness(1);
+
+            _timer = ThreadPoolTimer.CreatePeriodicTimer(Timer_Tick, TimeSpan.FromMilliseconds(2000));
+        }
+
+        private void Timer_Tick(ThreadPoolTimer timer)
+        {
+            ticks++;
+
+            if (ticks > Times)
+            {
+                _blinkt.Clear();
+                _deferral.Complete();
+            }
+            
+            if (_isOn)
+            {
+                _blinkt.SetAll(0, 255, 0, 1);
+            }
+            else
+            {
+                _blinkt.SetAll(255, 0, 0, 1);
+            }
+
+            
         }
     }
 }
